dbf0d8996cf6e1bf7366326086ae17bbdb78746e
mthdht <mthdht@gmail.com>
2025-04-23 10:26:10 +0200

    add vite config and package json

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'src/library.js'),
+      name: 'easy-date',
+      // the proper extensions will be added
+      fileName: 'easy-date',
+    },
+    rollupOptions: {
+    },
+  },
+})
